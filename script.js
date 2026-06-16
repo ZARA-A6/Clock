952db28c439d67475504747d96cac4f1ea4e27a8
ZARA-A6 <zahraahaidarii0909@gmail.com>
2026-06-16 15:32:10 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const hour = document.querySelector('.hour');
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+
+const deg= 6;
+
+setInterval(()=>{
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hour.style.setProperty('--rotate', hh + (mm/12) + 'deg'); +deg/12
+    min.style.setProperty('--rotate', mm + 'deg');
+    sec.style.setProperty('--rotate', ss + 'deg');
+
+})
